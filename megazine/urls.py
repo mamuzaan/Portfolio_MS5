f7d47f05c78247089b99f79da819f81e2fa854b3
Yakub Ali <mamuzaan@yahoo.com>
2023-02-18 12:03:13 +0000

    Create basket.py and urls.py to basket app and connect to project label urls

--- megazine/urls.py
+++ megazine/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('store.urls', namespace='store')),
+    path('basket/', include('basket.urls', namespace='basket')),
 ]
 
 if settings.DEBUG:
