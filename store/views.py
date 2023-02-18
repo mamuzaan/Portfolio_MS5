341b6a9bbf64746651484e7dca08f7df26c8f8c8
Yakub Ali <mamuzaan@yahoo.com>
2023-02-18 05:55:53 +0000

    Move categories function from views to context_processors.py in store app

--- store/views.py
+++ store/views.py
@@ -2,12 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 
-def categories(request):
-    return {
-        'categories': Category.objects.all()
-    }
-
-
 def all_products(request):
     products = Product.objects.all()
     return render(request, "store/home.html", {'products': products})
