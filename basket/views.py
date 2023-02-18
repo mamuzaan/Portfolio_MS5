8987dbc0371444157a0d6ec71c114c2edaad60c2
Yakub Ali <mamuzaan@yahoo.com>
2023-02-18 22:54:58 +0000

    Added __len__ to basket_add function for basket product quantity

--- basket/views.py
+++ basket/views.py
@@ -15,5 +15,6 @@ def basket_add(request):
         product_qty = int(request.POST.get('productqty'))
         product = get_object_or_404(Product, id=product_id)
         basket.add(product=product, qty=product_qty)
-        response = JsonResponse({'qty': product_qty})
+        basketqty = basket.__len__()
+        response = JsonResponse({'qty': basketqty})
         return response
