128417cb55a83be91e7dfc315f59167c1daf7a9f
Yakub Ali <mamuzaan@yahoo.com>
2023-02-19 00:50:14 +0000

    Added get_total_price function in basket.py and beautyfy sub total div in summary template

--- basket/basket.py
+++ basket/basket.py
@@ -48,3 +48,6 @@ class Basket():
         Get the basket data and count the qty of items
         """
         return sum(item['qty'] for item in self.basket.values())
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
